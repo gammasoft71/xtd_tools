f1028dcb15348c9e7f1b32c3bba5a174f99c13d6
Gammasoft <gammasoft71@gmail.com>
2023-11-11 20:09:32 +0100

    Fix main

--- xtd_tools/properties/target_properties.cmake
+++ xtd_tools/properties/target_properties.cmake
@@ -1,4 +1,4 @@
 target_default_namespace("xtd_tools")
 target_icon(resources/xtd_tools)
 target_name("xtd_tools")
-target_startup("xtd_tools::main_form" src/main_form.h)
+target_startup("xtd_tools::main_form::main" src/main_form.h)
